5a37a4082999f110f0dd76b128ebeb8317b72f73
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2025-08-15 08:09:00 +0700

    Add getComputerchoice function
    Implemented getComputerChoice() to randomly return one of "rock",
    "paper", or "scissors".

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 0) return "rock";
+  if (computerChoice === 1) return "paper";
+  if (computerChoice === 2) return "scissors";
+}
